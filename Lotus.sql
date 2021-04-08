8db43f612cdd5ccb7c192f6eb26732e986f12674
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-04-08 00:59:48 -0700

    start db schema

---
+++ Lotus.sql
@@ -0,0 +1,346 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 24, 2020 at 04:06 PM
+-- Server version: 5.7.28-0ubuntu0.18.04.4
+-- PHP Version: 7.1.33-9+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Lotus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `secret` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unused',
+  `account_id` int(11) NOT NULL,
+  `is_admin` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `youtube` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `twitter` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `twitch` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `discord` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `register_date` int(11) NOT NULL DEFAULT '0',
+  `privileges` int(6) NOT NULL DEFAULT '126',
+  `is_bot` int(1) NOT NULL DEFAULT '0', -- FIX THIS
+  `user_id` int(11) NOT NULL,
+  `stars` int(11) NOT NULL DEFAULT '0',
+  `demons` int(11) NOT NULL DEFAULT '0',
+  `icon` int(11) NOT NULL DEFAULT '0',
+  `color1` int(11) NOT NULL DEFAULT '0',
+  `color2` int(11) NOT NULL DEFAULT '3',
+  `icon_type` int(11) NOT NULL DEFAULT '0',
+  `coins` int(11) NOT NULL DEFAULT '0',
+  `user_coins` int(11) NOT NULL DEFAULT '0',
+  `icon` int(11) NOT NULL DEFAULT '0',
+  `ship` int(11) NOT NULL DEFAULT '0',
+  `ball` int(11) NOT NULL DEFAULT '0',
+  `bird` int(11) NOT NULL DEFAULT '0',
+  `dart` int(11) NOT NULL DEFAULT '0',
+  `robot` int(11) DEFAULT '0',
+  `glow` int(11) NOT NULL DEFAULT '0',
+  `cp` double NOT NULL DEFAULT '0',
+  `ip` varchar(69) COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1',
+  `last_played` int(11) NOT NULL DEFAULT '0',
+  `diamonds` int(11) NOT NULL DEFAULT '0',
+  `orbs` int(11) NOT NULL DEFAULT '0',
+  `completed_levels` int(11) NOT NULL DEFAULT '0',
+  `spider` int(11) NOT NULL DEFAULT '0',
+  `explosion` int(11) NOT NULL DEFAULT '0',
+  `banned` int(11) NOT NULL DEFAULT '0',
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+-- DO LATER
+CREATE TABLE `comments` (
+  `user_id` int(11) NOT NULL,
+  `userName` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `comment` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `secret` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none',
+  `level_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `percent` int(11) NOT NULL DEFAULT '0',
+  `isSpam` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `levels`
+--
+
+-- DO LATER
+
+CREATE TABLE `levels` (
+  `gameVersion` int(11) NOT NULL,
+  `binaryVersion` int(11) NOT NULL DEFAULT '0',
+  `userName` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `levelName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `levelDesc` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `levelVersion` int(11) NOT NULL,
+  `levelLength` int(11) NOT NULL DEFAULT '0',
+  `audioTrack` int(11) NOT NULL,
+  `auto` int(11) NOT NULL,
+  `password` int(11) NOT NULL,
+  `original` int(11) NOT NULL,
+  `twoPlayer` int(11) NOT NULL DEFAULT '0',
+  `song_id` int(11) NOT NULL DEFAULT '0',
+  `objects` int(11) NOT NULL DEFAULT '0',
+  `coins` int(11) NOT NULL DEFAULT '0',
+  `requestedStars` int(11) NOT NULL DEFAULT '0',
+  `extraString` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `levelString` longtext COLLATE utf8_unicode_ci,
+  `levelInfo` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `secret` mediumtext COLLATE utf8_unicode_ci NOT NULL,
+  `starDifficulty` int(11) NOT NULL DEFAULT '0' COMMENT '0=N/A 10=EASY 20=NORMAL 30=HARD 40=HARDER 50=INSANE 50=AUTO 50=DEMON',
+  `downloads` int(11) NOT NULL DEFAULT '0',
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `starDemon` int(1) NOT NULL DEFAULT '0',
+  `starAuto` int(2) NOT NULL DEFAULT '0',
+  `starStars` int(11) NOT NULL DEFAULT '0',
+  `uploadDate` varchar(1337) COLLATE utf8_unicode_ci NOT NULL,
+  `updateDate` bigint(11) NOT NULL,
+  `rateDate` bigint(20) NOT NULL DEFAULT '0',
+  `starCoins` int(11) NOT NULL DEFAULT '0',
+  `starFeatured` int(11) NOT NULL DEFAULT '0',
+  `starHall` int(11) NOT NULL DEFAULT '0',
+  `starEpic` int(11) NOT NULL DEFAULT '0',
+  `starDemonDiff` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `ext_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `unlisted` int(11) NOT NULL,
+  `originalReup` int(11) NOT NULL DEFAULT '0' COMMENT 'used for levelReupload.php',
+  `hostname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `isCPShared` int(11) NOT NULL DEFAULT '0',
+  `isDeleted` int(11) NOT NULL DEFAULT '0',
+  `isLDM` int(11) NOT NULL DEFAULT '0',
+  `awarded` int(11) NOT NULL DEFAULT '0',
+  `magic` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `levelscores`
+--
+
+-- DO LATER
+
+CREATE TABLE `levelscores` (
+  `score_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `percent` int(11) NOT NULL,
+  `uploadDate` int(11) NOT NULL,
+  `attempts` int(11) NOT NULL DEFAULT '0',
+  `coins` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privilegegroups`
+--
+
+CREATE TABLE `privilegegroups` (
+  `id` int(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `privileges` bigint(20) NOT NULL,
+  `color` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+-- DO LATER
+
+CREATE TABLE `users` (
+  `isRegistered` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ext_id` int(100) NOT NULL,
+  `userName` varchar(69) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'undefined',
+  `stars` int(11) NOT NULL DEFAULT '0',
+  `demons` int(11) NOT NULL DEFAULT '0',
+  `icon` int(11) NOT NULL DEFAULT '0',
+  `color1` int(11) NOT NULL DEFAULT '0',
+  `color2` int(11) NOT NULL DEFAULT '3',
+  `iconType` int(11) NOT NULL DEFAULT '0',
+  `coins` int(11) NOT NULL DEFAULT '0',
+  `userCoins` int(11) NOT NULL DEFAULT '0',
+  `special` int(11) NOT NULL DEFAULT '0',
+  `gameVersion` int(11) NOT NULL DEFAULT '0',
+  `secret` varchar(69) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'none',
+  `accIcon` int(11) NOT NULL DEFAULT '0',
+  `accShip` int(11) NOT NULL DEFAULT '0',
+  `accBall` int(11) NOT NULL DEFAULT '0',
+  `accBird` int(11) NOT NULL DEFAULT '0',
+  `accDart` int(11) NOT NULL DEFAULT '0',
+  `accRobot` int(11) DEFAULT '0',
+  `accGlow` int(11) NOT NULL DEFAULT '0',
+  `creatorPoints` double NOT NULL DEFAULT '0',
+  `IP` varchar(69) COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1',
+  `lastPlayed` int(11) NOT NULL DEFAULT '0',
+  `diamonds` int(11) NOT NULL DEFAULT '0',
+  `orbs` int(11) NOT NULL DEFAULT '0',
+  `completedLvls` int(11) NOT NULL DEFAULT '0',
+  `accSpider` int(11) NOT NULL DEFAULT '0',
+  `accExplosion` int(11) NOT NULL DEFAULT '0',
+  `chest1time` int(11) NOT NULL DEFAULT '0',
+  `chest2time` int(11) NOT NULL DEFAULT '0',
+  `chest1count` int(11) NOT NULL DEFAULT '0',
+  `chest2count` int(11) NOT NULL DEFAULT '0',
+  `isBanned` int(11) NOT NULL DEFAULT '0',
+  `isCreatorBanned` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaderboard`
+--
+
+CREATE TABLE `leaderboard` (
+  `position` int(10) unsigned NOT NULL, 
+  `user_id` int(11) NOT NULL, 
+  PRIMARY KEY (`position`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`account_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `is_admin` (`is_admin`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `level_id` (`level_id`);
+
+--
+-- Indexes for table `levels`
+-- DO LATER
+ALTER TABLE `levels`
+  ADD PRIMARY KEY (`level_id`),
+  ADD KEY `level_id` (`level_id`),
+  ADD KEY `levelName` (`levelName`),
+  ADD KEY `starDifficulty` (`starDifficulty`),
+  ADD KEY `starFeatured` (`starFeatured`),
+  ADD KEY `starEpic` (`starEpic`),
+  ADD KEY `starDemonDiff` (`starDemonDiff`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `likes` (`likes`),
+  ADD KEY `downloads` (`downloads`),
+  ADD KEY `starStars` (`starStars`),
+  ADD KEY `song_id` (`song_id`),
+  ADD KEY `audioTrack` (`audioTrack`),
+  ADD KEY `levelLength` (`levelLength`),
+  ADD KEY `twoPlayer` (`twoPlayer`);
+
+--
+-- Indexes for table `levelscores`
+--
+ALTER TABLE `levelscores`
+  ADD PRIMARY KEY (`score_id`),
+  ADD KEY `level_id` (`level_id`);
+
+--
+-- Indexes for table `privilegegroups`
+--
+ALTER TABLE `privilegegroups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+-- DO LATER
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `userName` (`userName`),
+  ADD KEY `stars` (`stars`),
+  ADD KEY `demons` (`demons`),
+  ADD KEY `coins` (`coins`),
+  ADD KEY `userCoins` (`userCoins`),
+  ADD KEY `gameVersion` (`gameVersion`),
+  ADD KEY `creatorPoints` (`creatorPoints`),
+  ADD KEY `diamonds` (`diamonds`),
+  ADD KEY `orbs` (`orbs`),
+  ADD KEY `completedLvls` (`completedLvls`),
+  ADD KEY `isBanned` (`isBanned`),
+  ADD KEY `isCreatorBanned` (`isCreatorBanned`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+--
+-- AUTO_INCREMENT for table `levels`
+--
+ALTER TABLE `levels`
+  MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+--
+-- AUTO_INCREMENT for table `levelscores`
+--
+ALTER TABLE `levelscores`
+  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+==
+-- AUTO_INCREMENT for table `privilegegroups`
+--
+ALTER TABLE `privilegegroups`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
